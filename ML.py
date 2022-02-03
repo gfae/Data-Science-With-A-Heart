97234e582b71d0774968fca518c26621bb7ea2d1
gfae <gfaekeller@gmail.com>
2022-02-03 15:52:24 +0000

    Cleaned up code

--- ML.py
+++ ML.py
@@ -6,12 +6,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import MinMaxScaler
 sns.set(style='darkgrid')
-def ML(finaldf):
+
+def ML():
     currentDir = dirname(__file__)
     filePath = join(currentDir, "./heart.csv")
-    # with open(filePath, newline='') as csvfile:
-    #     testreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    #     for row in testreader:
     df = pd.read_csv(filePath)
     df.columns = ['Age', 'Sex', 'Chest_pain_type', 'Resting_bp',
                   'Cholesterol', 'Fasting_bs', 'Resting_ecg',
@@ -19,30 +17,11 @@ def ML(finaldf):
                   'ST_depression', 'ST_slope', 'Num_major_vessels',
                   'Thallium_test', 'Condition']
 
-    print(df)
-
     X = df.drop(['Condition'], axis=1)
     y = df.Condition
     X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                         test_size=0.2,
                                                         random_state=42)
-
-
-    print("\nX_train:\n")
-    print(X_train.head()) #prints the first 5 elements
-    print(X_train.shape) #prints number of rows & columns
-
-    print("\nX_test:\n")
-    print(X_test.head())
-    print(X_test.shape)
-
-    print("\ny_train:\n")
-    print(y_train.head())
-    print(y_train.shape)
-
-    print("\ny_test:\n")
-    print(y_test.head())
-    print(y_test.shape)
 
     def get_normalization(X):
         scaler = MinMaxScaler()
@@ -58,135 +37,21 @@ def ML(finaldf):
 
     X_train = get_normalization(X_train);
     X_test = get_normalization(X_test);
+    # X_fit = get_normalization(finaldf)
 
     lr = LogisticRegression()
     lr.fit(X_train, y_train)
-    f_pred = lr.predict(finaldf)
+
+    df2 = pd.DataFrame([[52,1,2,172,199,1,1,162,0,0.5,2,0,3],[48,0,2,130,275,0,1,139,0,0.2,2,0,2],[57,0,1,130,236,0,0,174,0,0,1,1,2],[68,1,0,144,193,1,1,141,0,3.4,1,2,3],[57,0,0,120,354,0,1,163,1,0.6,2,0,2]], columns=['Age', 'Sex', 'Chest_pain_type', 'Resting_bp',
+                                        'Cholesterol', 'Fasting_bs', 'Resting_ecg',
+                                        'Max_heart_rate', 'Exercise_induced_angina',
+                                        'ST_depression', 'ST_slope', 'Num_major_vessels',
+                                        'Thallium_test'])
+    X_fit = get_normalization(df2.values)
+    prediction = lr.predict(df2.values)
     logreg_acc = get_model_accuracy(lr, X_test, y_test)
-    print(f'Logistic Regression Accuracy: {logreg_acc:.4}')
+    # print(f'Logistic Regression Accuracy: {logreg_acc:.4}')
 
-    return (f_pred)
+    return (prediction)
 
-# drop row if all data is missing
-# can use 'any' to drop if any values are null but not needed as other data can still be used
-# will not corrupt the dataset
-# data = df.dropna(how='all').shape
-#
-# # print(data.info())
-# # print(data.isnull().sum())
-# def condition_ratio(data):
-#     """
-#     Makes 3 pie charts of 'Condition' values
-#     Condition: 0 = Benign, 1 = Malignant
-#     First is all data, second is female, third is male
-#     - results stores the total count of each condition variable
-#     - values stores a list of those values
-#     plt.pie creates the pie chart
-#         - values are self explanatory (autopct will show the percentage)
-#     """
-#     fig = plt.figure(figsize=(10, 8))
-#     plt.subplot(2,3,2)
-#     results = data['Condition'].value_counts()
-#     values = [results[0], results[1]]
-#     labels = ['Benign', 'Malignant']
-#     colours = ('DarkTurquoise', 'Salmon')
-#     plt.pie(values, labels = labels, colors=colours, autopct='%1.0f%%')
-#     plt.title('Benign vs. Malignant', fontsize=15)
-#     """
-#     Female condtion ratio pie chart
-#     """
-#     resultsBySex = data['Condition'].groupby(data['Sex']).value_counts()
-#     valuesBySex = [resultsBySex[0], resultsBySex[1]]
-#     plt.subplot(2,3,4)
-#     plt.pie(valuesBySex[0], labels = labels, colors=colours, autopct='%1.0f%%')
-#     plt.title('Female Condtion Ratio', fontsize=15)
-#     """"
-#     Male condtion ratio pie chart
-#     """
-#     plt.subplot(2,3,6)
-#     plt.pie(valuesBySex[1], labels = labels, colors=colours, autopct='%1.0f%%')
-#     plt.title('Male Condition Ratio', fontsize=15)
-#
-#     plt.show()
-#
-# def isAgeAFactor(data):
-#     """
-#     Plotting age and condtion against various factors.
-#     Benign, malignant and age variables are used in all the following scatter graphs so variables have been set for them.
-#     """
-#     benign = data.Condition == 0
-#     malignant = data.Condition == 1
-#     age_benign = data.Age[benign]
-#     age_malignant = data.Age[malignant]
-#     b_color = 'DarkTurquoise'
-#     m_color = 'Salmon'
-#     fig = plt.figure(figsize=(16, 10))
-#
-#     """
-#     Comparing age and resting blood pressure
-#
-#     subplot(x,y,z) sets the layout of the different graphs within the figure
-#         - x is the number of rows
-#         - y is the number of columns
-#         - z is the position of the graph currently being created/adjusted
-#     plt.scatter plots points on the graph using the arguments provided
-#         - x axis is designated the age
-#         - y axis is designated the factor being plotted
-#         - s is the size of the scatter points, a smaller number will allow you to see all the points more clearly but a larger number will highlight crowding better
-#         - c is the colour, different colours for malignant and benign
-#     """
-#     plt.subplot(2,2,1)
-#     plt.scatter(x=age_benign, y=data.Resting_bp[benign], s=40, c=b_color)
-#     plt.scatter(x=age_malignant, y=data.Resting_bp[malignant], s=40, c=m_color)
-#     plt.title('Resting BP vs. age', fontsize=15)
-#     plt.legend(['Benign', 'Malignant'])
-#     plt.xlabel('age', fontsize=10)
-#     plt.ylabel('Resting blood pressure (mmHg)', fontsize=10)
-#
-#
-#     """"
-#     Comparing age and cholesterol
-#     """
-#     plt.subplot(2,2,2)
-#     plt.scatter(x=age_benign, y=data.Cholesterol[benign], s=40, c=b_color)
-#     plt.scatter(x=age_malignant, y=data.Cholesterol[malignant], s=40, c=m_color)
-#     plt.title('Cholesterol vs. age', fontsize=15)
-#     plt.legend(['Benign', 'Malignant'])
-#     plt.xlabel('age', fontsize=10)
-#     plt.ylabel('Serum cholestoral (mg/dL)', fontsize=10)
-#
-#     """
-#     Comparing maximum heart rate and age
-#     """
-#     plt.subplot(2,2,3)
-#     plt.scatter(x=age_benign, y=data.Max_heart_rate[benign], s=40, c=b_color)
-#     plt.scatter(x=age_malignant, y=data.Max_heart_rate[malignant], s=40, c=m_color)
-#     plt.title('Maximum heart rate vs. age', fontsize=15)
-#     plt.legend(['Benign', 'Malignant'])
-#     plt.xlabel('age', fontsize=10)
-#     plt.ylabel('Max heart rate (bpm)', fontsize=10)
-#
-#
-#     """
-#     Comparing ST Depression and age
-#
-#     When performing an ekg, the flat part between the peaks is called the ST segment,
-#     The heart isn't doing anything between pumps so this part should be the same as the baseline
-#     When the reading is lower than expected, this is called ST depression and can be a sympton of heart disease.
-#     """
-#     plt.subplot(2,2,4)
-#     plt.scatter(x=age_benign, y=data.ST_depression[benign], s=40, c=b_color)
-#     plt.scatter(x=age_malignant, y=data.ST_depression[malignant], s=40, c=m_color)
-#     plt.title('ST Depression vs. age', fontsize=15)
-#     plt.legend(['Benign', 'Malignant'])
-#     plt.xlabel('age', fontsize=10)
-#     plt.ylabel('ST Depression', fontsize=10)
-#
-#     plt.tight_layout()
-#     plt.show()
-#
-#
-# condition_ratio(df)
-# isAgeAFactor(df)
-#
-#
+print(ML())
--- inputTest.py
+++ inputTest.py
@@ -4,45 +4,28 @@ import numpy as np
 from PyQt6.QtCore import QRegularExpression
 from PyQt6.QtGui import QRegularExpressionValidator
 from PyQt6.QtWidgets import QMessageBox
-
-
 from ML import ML
 
 def readData():
     age = call.lineEdit_Age.text()
-    sexIndex, sex = call.comboBox_Sex.currentIndex(), call.comboBox_Sex.currentText()
-    CPIndex, CP = call.comboBox_CP.currentIndex(), call.comboBox_CP.currentText()
+    sex = call.comboBox_Sex.currentIndex()
+    CP = call.comboBox_CP.currentIndex()
     BP = call.lineEdit_BP.text()
     chol = call.lineEdit_Chol.text()
     FBS = call.comboBox_FBS.currentIndex()
-    ECGIndex, ECG = call.comboBox_ECG.currentIndex(), call.comboBox_ECG.currentText()
+    ECG = call.comboBox_ECG.currentIndex()
     HR = call.lineEdit_HR.text()
-    EIAIndex, EIA = call.comboBox_EIA.currentIndex(), call.comboBox_EIA.currentText()
+    EIA = call.comboBox_EIA.currentIndex()
     STD = call.lineEdit_STP.text()
-    STSIndex, STS = call.comboBox_STS.currentIndex(), call.comboBox_STS.currentText()
-    CMBVIndex, CMBV = call.comboBox_CMBV.currentIndex(), call.comboBox_CMBV.currentText()
-    thalIndex, thal = call.comboBox_Thal.currentIndex(), call.comboBox_Thal.currentText()
+    STS = call.comboBox_STS.currentIndex()
+    CMBV = call.comboBox_CMBV.currentIndex()
+    thal = call.comboBox_Thal.currentIndex()
 
-    dfData = [age, sexIndex, CPIndex, BP, chol, FBS, ECGIndex, HR, EIAIndex, STD, STSIndex, CMBVIndex, thalIndex]
     data = [age, sex, CP, BP, chol, FBS, ECG, HR, EIA, STD, STS, CMBV, thal]
-    # for i in range(len(data)):
-    #     if data[i] == -1 or data[i] == "":
-    #         data[i] = np.nan
-    #     else:
-    #         if data[i].isdigit():
-    #             data[i] = int(data[i])
-    #         else:
-    #             data[i] = float(data[i])
-    for i in range(len(dfData)):
-        if dfData[i] == -1 or dfData[i] == "":
-            dfData[i] = np.nan
-        else:
-            if dfData[i].isdigit():
-                dfData[i] = int(dfData[i])
-            else:
-                dfData[i] = float(dfData[i])
-    # showResults(data)
-    return createDf(dfData)
+    for i in range(len(data)):
+        if data[i] == -1 or data[i] == "":
+            data[i] = np.nan
+    return createDf(data)
 
 def createDf(data):
     print(data)
@@ -52,28 +35,11 @@ def createDf(data):
                                         'ST_depression', 'ST_slope', 'Num_major_vessels',
                                         'Thallium_test'])
     print (df2)
-    # result = ML(df2)
-    # print(result)
+    result = ML(df2)
+    return showResults(result)
 
-def showResults(data):
+def showResults(result):
     msg = QMessageBox()
-    result = data
-    # maybe use later?
-    # msg.setText(f"<html><body><h3>Patient details</h3>"
-    #             f"<p>Age: {data[0]}<br>"
-    #             f"Sex: {data[1]}<br>"
-    #             f"Chest pain type: {data[2]}<br>"
-    #             f"Resting BP: {data[3]}mmHG<br>"
-    #             f"Cholesterol: {data[4]}mg/dL<br>"
-    #             f"Fasting blood sugar: {data[5]}mg/dL<br>"
-    #             f"Resting ECG: {data[6]}<br>"
-    #             f"Max HR: {data[7]}bpm<br>"
-    #             f"Exercise induced angina: {data[8]}<br>"
-    #             f"ST depression: {data[9]}<br>"
-    #             f"ST slop: {data[10]}<br>"
-    #             f"Major blood vessels: {data[11]}<br>"
-    #             f"Thalium test: {data[12]}</p>"
-    #             f"</body></html>")
     msg.setText(f"Result: {result}")
     msg.setWindowTitle("Results")
     msg.exec()
