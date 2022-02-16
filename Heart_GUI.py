a5a97fcb77185b51bb7aa934a67502bc17fe579b
gfae <gfaekeller@gmail.com>
2022-02-16 09:01:27 +0000

    Renamed testUI.ui to UI_Design.ui

--- Heart_GUI.py
+++ Heart_GUI.py
@@ -86,7 +86,7 @@ def clearData():
 
 
 app = QtWidgets.QApplication([])
-call = uic.loadUi("UI_Design.ui")
+call = uic.loadUi("testUI.ui")
 
 validator = QRegularExpressionValidator(QRegularExpression(r'[0-9]+'))
 decimalValidator = QRegularExpressionValidator(QRegularExpression(r'[0-9]+\.[0-9]'))
